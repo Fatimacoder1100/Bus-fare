476229fd85029e83cb8f5b92bab8a6f928db7b1b
Fatimacoder1100 <fatimaumerbhalli@gmail.com>
2025-11-04 08:14:57 -0800

    Initialize commit

---
+++ main.py
@@ -0,0 +1,17 @@
+class Vehicle:
+    def __init__(self, name, max_speed, mileage):
+        self.name = name
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+class Bus(Vehicle):
+    pass
+
+school_bus = Bus("School Volvo", 80, 20)
+fare_per_km = 10
+distance = float(input("Enter distance in km: "))
+total_fare = distance * fare_per_km
+print("Vehicle Name:", school_bus.name)
+print("Speed:", school_bus.max_speed)
+print("Mileage:", school_bus.mileage)
+print("Total Fare:", total_fare, "rupees")
